5850ca074024d81739c83b4a0a9ca7ac6da9296f
Mate Andras Magyar <mate.magyar@ukatemi.com>
2023-04-05 12:00:22 +0200

    Remove legacy code paths

--- lib/core/src/app.h
+++
@@ -1,9 +0,0 @@
-#ifndef APP_H
-#define APP_H
-
-#include "platform/platform.h"
-
-int app_setup();
-int app_loop();
-
-#endif
--- lib/core/src/platform/log.cpp
+++
@@ -1,3 +0,0 @@
-#include "platform/log.h"
-
-char printf_buffer[LOG_PRINTF_BUF_SIZE];
