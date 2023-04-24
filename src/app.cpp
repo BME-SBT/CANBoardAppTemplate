6d40f289c239587e6a5cc6e046a73f22d224979b
Mate Andras Magyar <mate.magyar@ukatemi.com>
2023-04-24 12:32:07 +0200

    Working on measurements

--- src/app.cpp
+++ src/app.cpp
@@ -1,22 +1,23 @@
 #include "app.h"
+#include <api/Common.h>
+#include <cstdio>
+#include <cstdlib>
 
 #include "lib/inttypes.h"
 
-PlatformStatus app_setup() { return PlatformStatus::STATUS_OK; }
+PlatformStatus app_setup() {
+    srand(micros());
+    return PlatformStatus::STATUS_OK;
+}
 u64 counter = 0;
-u16 id = 0;
-u64 last_sent = 0;
-u64 send_delay = 100;
+
 
 PlatformStatus app_loop() {
-
+    LOG("app loop");
+    u16 id = rand() % 0x7ff;
     CAN_Frame frame(id, reinterpret_cast<u8 *>(&counter), 8);
-
+    LOG("sending frame...");
     PLATFORM_CAN.send(frame);
-    PLATFORM_CAN.send(frame);
-    PLATFORM_CAN.send(frame);
-    PLATFORM_CAN.send(frame);
-
     counter += 1;
     id += 1;
     if (id > 0x7ff) {
@@ -24,7 +25,7 @@ PlatformStatus app_loop() {
     }
     while (PLATFORM_CAN.available()) {
         __attribute__((unused)) CAN_Frame rv = PLATFORM_CAN.get_frame();
-        LOGF("got frame: %d %p", rv.standard_id, *(uint64_t*) (rv.data));
+        //printf("got frame: %d %d\r\n", rv.standard_id, *reinterpret_cast<u64*>(rv.data));
     }
 
     return PlatformStatus::STATUS_OK;
