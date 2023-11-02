e7daea0a42327e7c1bc3049daa8a71ec39fea176
Mate Andras Magyar <mate.magyar@ukatemi.com>
2023-11-02 13:57:02 +0100

    WIP: Push latest code

--- src/app.cpp
+++ src/app.cpp
@@ -1,32 +1,17 @@
 #include "app.h"
-#include <api/Common.h>
-#include <cstdio>
-#include <cstdlib>
 
-#include "lib/inttypes.h"
+Measurement motor_rpm((u16)0b00000010101,10,new RPM_Datatype);
 
 PlatformStatus app_setup() {
-    srand(micros());
+    PLATFORM_MEASUREMENTS.add_measurement(&motor_rpm);
+
     return PlatformStatus::STATUS_OK;
 }
-u64 counter = 0;
 
+u16 counter = 0;
 
 PlatformStatus app_loop() {
-    LOG("app loop");
-    u16 id = rand() % 0x7ff;
-    CAN_Frame frame(id, reinterpret_cast<u8 *>(&counter), 8);
-    LOG("sending frame...");
-    PLATFORM_CAN.send(frame);
-    counter += 1;
-    id += 1;
-    if (id > 0x7ff) {
-        id = 0;
-    }
-    while (PLATFORM_CAN.available()) {
-        __attribute__((unused)) CAN_Frame rv = PLATFORM_CAN.get_frame();
-        //printf("got frame: %d %d\r\n", rv.standard_id, *reinterpret_cast<u64*>(rv.data));
-    }
-
+    motor_rpm.set_value(counter++);
+    delayMicroseconds(10);
     return PlatformStatus::STATUS_OK;
 }
